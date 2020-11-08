699f31131a72c886170d3a7f87829834f7ce9f27
Jeffrey Getzin <getzinj@gmail.com>
2020-11-07 23:59:37 -0500

    Express tests passing.

--- tests/express.spec.ts
+++ tests/express.spec.ts
@@ -1,45 +1,52 @@
-// 'use strict';
-//
-// var path = require('path');
-// var express = require('express');
-// var expect = require('expect.js');
-// var request = require('supertest');
-// var nunjucks = require('../nunjucks');
-//
-// var VIEWS = path.join(__dirname, '../samples/express/views');
-//
-// describe('express', function() {
-//   var app;
-//   var env;
-//
-//   beforeEach(function() {
-//     app = express();
-//     env = new nunjucks.Environment(new nunjucks.FileSystemLoader(VIEWS));
-//     env.express(app);
-//   });
-//
-//   it('should have reference to nunjucks env', function() {
-//     expect(app.settings.nunjucksEnv).to.be(env);
-//   });
-//
-//   it('should render a view with extension', function(done) {
-//     app.get('/', function(req, res) {
-//       res.render('about.html');
-//     });
-//     request(app)
-//       .get('/')
-//       .expect(/This is just the about page/)
-//       .end(done);
-//   });
-//
-//   it('should render a view without extension', function(done) {
-//     app.get('/', function(req, res) {
-//       res.render('about');
-//     });
-//     app.set('view engine', 'html');
-//     request(app)
-//       .get('/')
-//       .expect(/This is just the about page/)
-//       .end(done);
-//   });
-// });
+'use strict';
+
+import { Done } from 'mocha';
+
+const path = require('path');
+const express = require('express');
+const expect = require('expect.js');
+const request = require('supertest');
+const nunjucks = require('../nunjucks');
+
+const VIEWS = path.join(__dirname, '../samples/express/views');
+
+
+describe('express', () => {
+  let app;
+  let env;
+
+
+  beforeEach(() => {
+    app = express();
+    env = new nunjucks.Environment(new nunjucks.FileSystemLoader(VIEWS));
+    env.express(app);
+  });
+
+
+  it('should have reference to nunjucks env', () => {
+    expect(app.settings.nunjucksEnv).to.be(env);
+  });
+
+
+  it('should render a view with extension', (done: Done) => {
+    app.get('/', function(req, res) {
+      res.render('about.html');
+    });
+    request(app)
+      .get('/')
+      .expect(/This is just the about page/)
+      .end(done);
+  });
+
+
+  it('should render a view without extension', (done: Done) => {
+    app.get('/', function(req, res) {
+      res.render('about');
+    });
+    app.set('view engine', 'html');
+    request(app)
+      .get('/')
+      .expect(/This is just the about page/)
+      .end(done);
+  });
+});
