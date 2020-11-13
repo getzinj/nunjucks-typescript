8e89fe04fb7b4c416a9a4a9619e497bee2c57f58
Jeffrey Getzin <getzinj@gmail.com>
2020-11-13 16:25:03 -0500

    Ignore generated files.

--- nunjucks/src/lexer/characters.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.intChars = exports.delimChars = exports.whitespaceChars = void 0;
-exports.whitespaceChars = ' \n\t\r\u00A0';
-exports.delimChars = '()[]{}%*-+~/#,:|.<>=!';
-exports.intChars = '0123456789';
-//# sourceMappingURL=characters.js.map
--- tests/setup.spec.js
+++
@@ -1,3 +0,0 @@
-// process.env.NODE_ENV = 'test';
-//
-//# sourceMappingURL=setup.spec.js.map
