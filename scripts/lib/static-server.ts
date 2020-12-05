c6cdbef6c1d2f95bec7b8159f51d79c6c3256360
Jeffrey Getzin <getzinj@gmail.com>
2020-12-04 20:34:38 -0500

    Done for now.

--- scripts/lib/static-server.ts
+++ scripts/lib/static-server.ts
@@ -9,13 +9,13 @@ import { Server, RequestListener } from 'http';
 
 export function getStaticServer(port?: number): Promise<[Server, number]> {
   const staticRoot: string = path.join(__dirname, '../..');
-  const portPromise: Promise<number> = (typeof port === 'undefined') ? getPort.default() : Promise.resolve(port);
+  const portPromise: Promise<number> = (typeof port === 'undefined') ? getPort() : Promise.resolve(port);
 
   return portPromise.then( (port: number): Promise<[Server, number]> => { // eslint-disable-line no-shadow
     return new Promise<[ Server, number ]>(
         (resolve: (result: [ Server, number ]) => void, reject: (reason: any) => void): void => {
             try {
-              const app: RequestListener = connect.default().use(serveStatic.default(staticRoot));
+              const app: RequestListener = connect().use(serveStatic(staticRoot));
               const server: Server = http.createServer(app);
               server.listen(port, (): void => {
                 console.log(`Test server listening on port ${ port }`); // eslint-disable-line no-console
